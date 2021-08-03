3a4803c3749baa8be967e07b1c01983013cadf41
s594zhan <s594zhan@student.cs.uwaterloo.ca>
2021-08-02 21:32:17 -0400

    little change

--- sky/board.h
+++ sky/board.h
@@ -10,9 +10,10 @@ class Cell;
 class Board {
     public:
     int boardNo;
+    int score;
+    int level = 0; //TODO: is the default level set to 0?
     const int rows = 15 + 3;
     const int cols = 11;
-    int level = 0; //TODO: is the default level set to 0?
     bool random = false; // random is the random switch, default to false which is off.
     char specialAction = 'N'; // default N means None, can be replaced with B/H/F
     bool gameStatus = false; // game is still going, neither won nor over.
